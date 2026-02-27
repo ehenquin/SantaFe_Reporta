5a17fcdbe4057e4f1af9dd3f8adada0e7d0a4cd8
ehenquin <167160026+ehenquin@users.noreply.github.com>
2026-02-27 15:43:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 // 1. Importamos las herramientas necesarias
-import { guardarReporteDB, obtenerReportesDB } from './api.js';
+import { guardarReporteDB, obtenerReportesDB } from './api.js?v=1';
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // CONFIGURACIÓN DE SUPABASE (Necesaria para las funciones de admin/denuncia)
@@ -204,3 +204,4 @@ async function cargarReportes() {
 }
 // Ejecutar al inicio
 cargarReportes();
+
